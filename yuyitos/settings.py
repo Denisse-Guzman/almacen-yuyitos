a898c9bb0c9e7c5c25b2046075687d61fd32a289
Tu Nombre <tucorreo@ejemplo.com>
2025-12-10 00:07:15 -0300

    Agrega 127.0.0.1 a ALLOWED_HOSTS

--- yuyitos/settings.py
+++ yuyitos/settings.py
@@ -29,6 +29,7 @@ DEBUG = os.environ.get("DJANGO_DEBUG", "True") == "True"
 ALLOWED_HOSTS = [
     "almacen-yuyitos.onrender.com",  # dominio de Render
     "localhost",
+    "127.0.0.1",
 ]
 
 
--- yuyitos/urls.py
+++ yuyitos/urls.py
@@ -1,35 +1,36 @@
 """
 URL configuration for yuyitos project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+The `urlpatterns` list routes URLs to views.
 """
+
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 from cuentas import views as cuentas_views
 
+
+def raiz_a_login(request):
+    # Redirige a la vista de login
+    return redirect("login")
+
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-        # API
+    path("admin/", admin.site.urls),
+
+    # Raíz del sitio -> login
+    path("", raiz_a_login, name="raiz"),
+
+    # API
     path("api/", include("clientes.urls_api")),
     path("api/", include("ventas.urls_api")),
     path("api/", include("inventario.urls_api")),
     path("api/", include("proveedores.urls_api")),
-        #  Auth
+
+    # Auth
     path("login/", cuentas_views.login_view, name="login"),
     path("logout/", cuentas_views.logout_view, name="logout"),
     path("dashboard/caja/", cuentas_views.dashboard_caja, name="dashboard_caja"),
     path("dashboard/bodega/", cuentas_views.dashboard_bodega, name="dashboard_bodega"),
     path("dashboard/admin/", cuentas_views.dashboard_admin, name="dashboard_admin"),
 ]
-
